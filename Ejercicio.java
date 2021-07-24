360e4667813e1aa6477dfec5941c84d275090eb8
AngieCadena <87679488+AngieCadena@users.noreply.github.com>
2021-07-24 16:43:20 -0500

    Update Ejercicio.java
    Edicion desde GitHub

--- Ejercicio.java
+++ Ejercicio.java
@@ -5,6 +5,8 @@ import javax.print.attribute.standard.MediaName;
 public class Ejercicio {
     public static void name() {
     Scanner sc=new Scanner();
+        System.out.print("x");
+        
     }
     
 }
